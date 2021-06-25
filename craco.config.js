caca7473801a9342d7895cee1d4d630a12333915
lorof <gilyazov@list.ru>
2021-06-26 01:49:06 +0500

    fix aliases

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   webpack: {
     alias: {
-      '~': path.resolve(__dirname, './src')
+      '~': path.resolve(__dirname, './src'),
+      ...(isProd ? { 'effector-logger': 'effector' } : {})
     }
   }
 }
