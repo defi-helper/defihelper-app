12da6248c3566ddee27a12052c2b6f876b3842b1
Vladislav Komissarov <mastyf@gmail.com>
2021-07-19 17:12:20 +0300

    fix adapters host

--- src/staking/common/build-adapters-url.ts
+++ src/staking/common/build-adapters-url.ts
@@ -3,5 +3,5 @@ import { config } from '~/config'
 export const buildAdaptersUrl = (protocolAdapter?: string) => {
   if (!protocolAdapter) throw new Error('protocolAdapter is required')
 
-  return `${config.ADAPTERS_HOST}adapters/${protocolAdapter}.js`
+  return `${config.ADAPTERS_HOST}/${protocolAdapter}.js`
 }
