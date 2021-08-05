d037a8736de6ee25deaa54c65a0399358acf653d
lorof <gilyazov@list.ru>
2021-08-05 20:43:34 +0500

    add global deps for adapters

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import ReactDOM from 'react-dom'
 import 'normalize.css'
 import cachios from 'cachios'
+import dayjs from 'dayjs'
+import { BigNumber } from 'bignumber.js'
+import { ethers } from 'ethers'
 
 import { App } from './app'
 
+// For adapters
+window.dayjs = dayjs
+window.bignumber = BigNumber
+window.ethers = ethers
 window.axios = cachios
 
 ReactDOM.render(<App />, document.getElementById('root'))
