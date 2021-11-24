b6837a1ab643e1a35e2c383d49109f59bae7ec7c
lorof <gilyazov@list.ru>
2021-11-24 14:32:55 +0500

    fix

--- src/settings/settings-transaction-history/settings-transaction-history.tsx
+++ src/settings/settings-transaction-history/settings-transaction-history.tsx
@@ -207,7 +207,8 @@ export const SettingsTransactionHistory: React.VFC<SettingsTransactionHistoryPro
                     </Typography>
                     <Typography variant="body2" as="div">
                       {bignumberUtils.format(historyItem.amount, 6)}{' '}
-                      {networksConfig[historyItem.network].coin}
+                      {networksConfig[historyItem.network] &&
+                        networksConfig[historyItem.network].coin}
                     </Typography>
                     <Typography variant="body2" as="div">
                       {dateUtils.format(historyItem.createdAt)}
