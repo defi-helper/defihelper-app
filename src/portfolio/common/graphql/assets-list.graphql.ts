503fd09cd53dba32a3f7c85c2ac3d6ab10d62a80
Luka Nikitin <lukentuichennel@gmail.com>
2021-12-14 18:12:57 +0300

    fix

--- src/portfolio/common/graphql/assets-list.graphql.ts
+++ src/portfolio/common/graphql/assets-list.graphql.ts
@@ -4,7 +4,10 @@ import { PORTFOLIO_ASSET_FRAGMENT } from '~/portfolio/common/graphql/portfolio-a
 export const ASSETS_LIST = gql`
   query AssetList {
     me {
-      tokenAliases(pagination: { limit: 50 }) {
+      tokenAliases(
+        pagination: { limit: 50 }
+        filter: { liquidity: [stable, unstable] }
+      ) {
         list {
           ...portfolioAsset
         }
