f79890ddc1676aaf113e2545927f492a39478d45
Luka Nikitin <lukentuichennel@gmail.com>
2022-01-06 11:02:53 +0300

    fix

--- src/common/head/head.tsx
+++ src/common/head/head.tsx
@@ -5,7 +5,7 @@ import OpenGraph from 'src/assets/images/opengraph.jpg'
 export type HeadProps = {
   title?: string
   description?: string
-  ogImage?: string
+  ogImageUrl?: string
   ogUrl?: string
 }
 
@@ -18,12 +18,10 @@ export const Head: React.VFC<HeadProps> = (props) => {
   const siteTitle = ['DeFiHelper', props.title].filter(Boolean).join(' - ')
 
   const {
-    ogImage = OpenGraph,
+    ogImageUrl = OpenGraph,
     ogUrl = SITE_URL,
     description = SITE_DESCRIPTION,
   } = props
-
-  const image = [ogUrl, ogImage].join('')
 
   return (
     <Helmet>
@@ -33,8 +31,8 @@ export const Head: React.VFC<HeadProps> = (props) => {
       <meta property="og:description" content={description} />
       <meta property="og:url" content={ogUrl} />
       <meta name="twitter:url" content={ogUrl} />
-      <meta property="og:image" content={image} />
-      <meta name="twitter:image" content={image} />
+      <meta property="og:image" content={ogImageUrl} />
+      <meta name="twitter:image" content={ogImageUrl} />
       <meta property="og:site_name" content={siteTitle} />
       <meta name="twitter:title" content={siteTitle} />
     </Helmet>
