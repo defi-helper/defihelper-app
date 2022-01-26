cee84b3453a7388b802737ad5b8c1f0ed46c2bc9
lorof <gilyazov@list.ru>
2022-01-26 15:53:49 +0500

    remove comments

--- src/staking/common/staking.types.ts
+++ src/staking/common/staking.types.ts
@@ -36,9 +36,6 @@ export type ConnectParams = {
 
 export type Contract = StakingContractFragmentFragment & {
   type: 'Contract'
-  // syncedBlock: number
-  // scannerId?: string
-  // prototypeAddress?: string
   autostakingLoading?: boolean
 }
 
