0006e07df0fdef058238d5f0928acb2c0b4bde2a
Vladislav Komissarov <mastyf@gmail.com>
2022-01-29 21:48:11 +0300

    Misc text fixes

--- src/automations/common/automation-deploy-steps-dialog/automation-deploy-steps-dialog.tsx
+++ src/automations/common/automation-deploy-steps-dialog/automation-deploy-steps-dialog.tsx
@@ -125,8 +125,8 @@ export const AutomationDeployStepsDialog: React.FC<AutomationDeployStepsDialogPr
                       variant="body3"
                       className={styles.advancedAttention}
                     >
-                      Attention! Change it if you only know what to change.
-                      You can loose your funds.
+                      Attention! Change it if you only know what to change. You
+                      can loose your funds.
                     </Typography>
                   </>
                 ) : (
