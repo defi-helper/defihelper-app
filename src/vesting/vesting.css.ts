8dc5d63d35a4172c90bfbcfd30317f8b63669073
lorof <gilyazov@list.ru>
2022-04-13 12:54:54 +0300

    wip: vesting

--- src/vesting/vesting.css.ts
+++ src/vesting/vesting.css.ts
@@ -1,3 +1,33 @@
 import { style } from '@vanilla-extract/css'
 
-export const root = style({})
+import { theme } from '~/common/theme'
+
+export const root = style({
+  maxWidth: 400,
+  padding: 16,
+
+  '@media': {
+    [theme.mediaQueries.md()]: {
+      padding: '32px 32px 24px',
+    },
+  },
+})
+
+export const title = style({
+  marginBottom: 16,
+
+  '@media': {
+    [theme.mediaQueries.md()]: {
+      marginBottom: 32,
+    },
+  },
+})
+
+export const label = style({
+  color: theme.colors.textColorGrey,
+  marginBottom: 4,
+})
+
+export const row = style({
+  marginBottom: 16,
+})
--- src/vesting/vesting.tsx
+++ src/vesting/vesting.tsx
@@ -2,9 +2,12 @@ import { useAsyncFn, useAsyncRetry } from 'react-use'
 import { bignumberUtils } from '~/common/bignumber-utils'
 
 import { Button } from '~/common/button'
+import { Paper } from '~/common/paper'
+import { Typography } from '~/common/typography'
 import { AppLayout } from '~/layouts'
 import { WalletConnect } from '~/wallets/wallet-connect'
 import { useWalletNetwork } from '~/wallets/wallet-networks/wallet-network.model'
+import * as styles from './vesting.css'
 
 export type VestingProps = unknown
 
@@ -20,20 +23,48 @@ export const Vesting: React.VFC<VestingProps> = () => {
 
   return (
     <AppLayout>
-      <WalletConnect fallback={<Button>Connect</Button>} />
-      {isOwner.loading && 'loading...'}
-      {isOwner.value === true && !isOwner.loading && (
-        <>
-          <div>period finish: {periodFinish.value}</div>
-          <div>earned: {bignumberUtils.format(earned.value)}</div>
-          <Button onClick={handleClaim} loading={claimState.loading}>
-            Claim
-          </Button>
-        </>
-      )}
-      {isOwner.value === false && !isOwner.loading && (
-        <div>you&apos;re not owner</div>
-      )}
+      <WalletConnect fallback={<Button>Connect</Button>}>
+        <Typography variant="h3" className={styles.title}>
+          Pre seed
+        </Typography>
+        <Paper radius={8} className={styles.root}>
+          {isOwner.loading && 'loading...'}
+          {isOwner.value === true && !isOwner.loading && (
+            <>
+              <div className={styles.row}>
+                <Typography variant="body2" className={styles.label}>
+                  Tokens left
+                </Typography>
+                <Typography variant="h4" transform="uppercase" family="mono">
+                  {bignumberUtils.format(earned.value)}
+                </Typography>
+              </div>
+              <div className={styles.row}>
+                <Typography variant="body2" className={styles.label}>
+                  Drop date
+                </Typography>
+                <Typography variant="h4" transform="uppercase" family="mono">
+                  xxx DFH / day
+                </Typography>
+              </div>
+              <div className={styles.row}>
+                <Typography variant="body2" className={styles.label}>
+                  Drop end
+                </Typography>
+                <Typography variant="h4" transform="uppercase" family="mono">
+                  {periodFinish.value}
+                </Typography>
+              </div>
+              <Button onClick={handleClaim} loading={claimState.loading}>
+                Claim
+              </Button>
+            </>
+          )}
+          {isOwner.value === false && !isOwner.loading && (
+            <>you&apos;re not owner</>
+          )}
+        </Paper>
+      </WalletConnect>
     </AppLayout>
   )
 }
