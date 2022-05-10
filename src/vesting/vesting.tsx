648b35a339fe85ad2a4cd48dc19037d9e05dc276
Vladislav Komissarov <mastyf@gmail.com>
2022-05-10 10:42:22 +0300

    prettier fix

--- src/vesting/vesting.tsx
+++ src/vesting/vesting.tsx
@@ -220,8 +220,7 @@ export const Vesting: React.VFC<VestingProps> = () => {
                     >
                       {dateUtils.format(
                         dateUtils.addDate(Number(dropEnd), 'seconds')
-                      )}
-                      &nbsp;
+                      )}&nbsp;
                       (at block: {String(currentBlockNumber.value ?? 0)})
                     </Typography>
                   </div>
