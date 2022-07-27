099115dce5cc1e586096d82126ef6f054006e348
Luka Nikitin <lukentuichennel@gmail.com>
2022-07-27 11:36:37 +0200

    fix

--- src/portfolio/common/portfolio-asset-card/portfolio-asset-card.css.ts
+++ src/portfolio/common/portfolio-asset-card/portfolio-asset-card.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css'
 
 export const root = style({
   display: 'grid',
-  gridTemplateColumns: '10% 40% 20% 15% 15%',
+  gridTemplateColumns: '10% 30% 20% 10% 10% 10% 10%',
 
   selectors: {
     '&:not(:last-child)': {
