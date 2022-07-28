2d8c3e7b9e8d48fb5325b403f4ab8269c28d0380
Luka Nikitin <lukentuichennel@gmail.com>
2022-07-28 16:12:42 +0200

    fix lint

--- src/portfolio/common/portfolio-asset-card/portfolio-asset-card.tsx
+++ src/portfolio/common/portfolio-asset-card/portfolio-asset-card.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx'
 
 import React from 'react'
-import BigNumber from 'bignumber.js'
 import { bignumberUtils } from '~/common/bignumber-utils'
 import { Typography } from '~/common/typography'
 import {
