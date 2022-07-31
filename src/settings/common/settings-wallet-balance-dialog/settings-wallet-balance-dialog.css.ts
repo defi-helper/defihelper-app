616e64d8d07ee52cb3d7a95216fdc4bba5a8c3f8
lorof <gilyazov@list.ru>
2022-07-31 11:44:45 +0500

    wip: balance adapter

---
+++ src/settings/common/settings-wallet-balance-dialog/settings-wallet-balance-dialog.css.ts
@@ -0,0 +1,68 @@
+import { style } from '@vanilla-extract/css'
+
+import { theme } from '~/common/theme'
+
+export const root = style({
+  maxWidth: 460,
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  height: 704,
+  padding: 32,
+})
+
+export const mb = style({
+  marginBottom: 24,
+})
+
+export const title = style([
+  {
+    color: theme.colors.textColorGreen,
+    position: 'relative',
+
+    ':after': {
+      content: "''",
+      height: 1,
+      width: '100%',
+      position: 'absolute',
+      top: 'calc(100% + 4px)',
+      left: 0,
+      background: theme.colors.textColorGreen,
+    },
+  },
+])
+
+export const subtitle = style({
+  marginBottom: 48,
+})
+
+export const form = style({
+  flex: '1 0 auto',
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export const grey = style({
+  color: theme.colors.textColorGrey,
+})
+
+export const input = style({
+  marginBottom: 16,
+})
+
+export const balances = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 8,
+  marginBottom: 'auto',
+})
+
+export const recomendedBalance = style({
+  color: theme.colors.common.blue1,
+  textAlign: 'left',
+  justifyContent: 'flex-start',
+})
+
+export const button = style({
+  marginTop: 30,
+})
