98894626cf2d876e2be42d1add2a6774ca233de0
Luka Nikitin <lukentuichennel@gmail.com>
2022-09-23 16:13:54 +0200

    done

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
 import { HelmetProvider } from 'react-helmet-async'
 import { ClientContext } from 'graphql-hooks'
-import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
 
 import './app.css'
 import './assets/fonts/Basier-Circle-regular-webfont/stylesheet.css'
@@ -14,18 +13,11 @@ import { ToastProvider } from './toasts'
 import { AuthProvider } from './auth'
 import { getAPIClient } from './api'
 import { ErrorBoundary, Sentry } from './error-boundary'
-import { config } from './config'
 import { Cookie } from './common/cookie'
 
 Sentry.init()
 
 const client = getAPIClient()
-
-const matomoCounterConfiguration = createInstance({
-  urlBase: config.MATOMO_URL,
-  siteId: config.MATOMO_SITE_ID,
-  disabled: config.IS_DEV,
-})
 
 export const App: React.VFC = () => {
   useEthereumNetwork()
@@ -33,20 +25,18 @@ export const App: React.VFC = () => {
   return (
     <ThemeProvider>
       <ErrorBoundary>
-        <MatomoProvider value={matomoCounterConfiguration}>
-          <ClientContext.Provider value={client}>
-            <HelmetProvider>
-              <DialogProvider>
-                <ToastProvider maxItems={6}>
-                  <AuthProvider>
-                    <Router />
-                    <Cookie />
-                  </AuthProvider>
-                </ToastProvider>
-              </DialogProvider>
-            </HelmetProvider>
-          </ClientContext.Provider>
-        </MatomoProvider>
+        <ClientContext.Provider value={client}>
+          <HelmetProvider>
+            <DialogProvider>
+              <ToastProvider maxItems={6}>
+                <AuthProvider>
+                  <Router />
+                  <Cookie />
+                </AuthProvider>
+              </ToastProvider>
+            </DialogProvider>
+          </HelmetProvider>
+        </ClientContext.Provider>
       </ErrorBoundary>
     </ThemeProvider>
   )
