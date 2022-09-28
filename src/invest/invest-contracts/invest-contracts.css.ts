ed715c486de774c624ac487ccbb2cdaa89c3bc1f
lorof <gilyazov@list.ru>
2022-09-28 16:47:54 +0500

    change responsive

--- src/invest/invest-contracts/invest-contracts.css.ts
+++ src/invest/invest-contracts/invest-contracts.css.ts
@@ -17,6 +17,10 @@ export const selects = style({
   gap: 8,
 
   '@media': {
+    [theme.mediaQueries.md()]: {
+      gridTemplateColumns: '1fr 1fr 1fr 1fr',
+    },
+
     [theme.mediaQueries.lg()]: {
       gap: 16,
       gridTemplateColumns: '236px 236px 236px 1fr',
@@ -61,7 +65,7 @@ export const table = style({
   '@media': {
     [theme.mediaQueries.md()]: {
       background: theme.colors.paper,
-      minWidth: 1000,
+      minWidth: 1100,
     },
   },
 })
