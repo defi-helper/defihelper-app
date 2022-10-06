f4b7577dc512c791f62e23fc9482f591c71d8fe4
lorof <gilyazov@list.ru>
2022-10-06 17:57:21 +0500

    wip: apy dialog

--- src/staking/common/staking-apy-dialog/staking-apy-dialog.css.ts
+++ src/staking/common/staking-apy-dialog/staking-apy-dialog.css.ts
@@ -3,8 +3,51 @@ import { style } from '@vanilla-extract/css'
 import { theme } from '~/common/theme'
 
 export const root = style({
-  width: 460,
-  padding: '24px 32px',
+  width: 352,
+  padding: 32,
+})
+
+export const title = style({
+  color: theme.colors.textColorGreen,
+  marginBottom: 16,
+})
+
+export const tabs = style({})
+
+export const tabsTitle = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: 7,
+  marginBottom: 4,
+  color: theme.colors.textColorGrey,
+})
+
+export const tabsPaper = style({
+  background: theme.colors.tertiary,
+  borderRadius: 8,
+  padding: 2,
+  display: 'grid',
+  gap: 4,
+  gridTemplateColumns: '1fr 1fr 1fr 1fr',
+})
+
+export const tabsItem = style({
+  padding: '6px 0',
+  borderRadius: 6,
+})
+
+export const tabsItemActive = style({
+  background: theme.colors.common.green2,
+  color: theme.colors.common.white1,
+})
+
+export const mb16 = style({
+  marginBottom: 16,
+})
+
+export const howItWorks = style({
+  fontSize: 12,
+  lineHeight: '16px',
 })
 
 export const row = style({
--- src/staking/common/staking-apy-dialog/staking-apy-dialog.tsx
+++ src/staking/common/staking-apy-dialog/staking-apy-dialog.tsx
@@ -1,5 +1,13 @@
-import { bignumberUtils } from '~/common/bignumber-utils'
+import clsx from 'clsx'
+import { useState } from 'react'
+
+// import { bignumberUtils } from '~/common/bignumber-utils'
+import { Button } from '~/common/button'
+import { ButtonBase } from '~/common/button-base'
 import { Dialog } from '~/common/dialog'
+import { Icon } from '~/common/icon'
+import { Link } from '~/common/link'
+import { NumericalInput } from '~/common/numerical-input'
 import { Typography } from '~/common/typography'
 import * as styles from './staking-apy-dialog.css'
 
@@ -7,43 +15,161 @@ export type StakingApyDialogProps = {
   apr: Record<string, string>
   staked: string
   onCancel: () => void
+  onConfirm: () => void
+}
+
+// {apr.map((aprItem) => (
+//   <div className={styles.row} key={aprItem.title}>
+//     <Typography variant="body2">{aprItem.title}</Typography>
+//     <Typography variant="body2" family="mono" align="right">
+//       {bignumberUtils.formatMax(aprItem.apy, 10000)}%
+//     </Typography>
+//     <Typography variant="body2" family="mono" align="right">
+//       ${bignumberUtils.format(aprItem.perStaked)}
+//     </Typography>
+//   </div>
+// ))}
+
+const StakedFor: Record<string, string> = {
+  '1D': '1D',
+  '7D': '7D',
+  '30D': '30D',
+  '1Y': '1Y',
+}
+
+const CompoundingEvery: Record<string, string> = {
+  AUTO: 'AUTO',
+  '1D': '1D',
+  '7D': '7D',
+  '14D': '14D',
+}
+
+const Tabs = (props: {
+  children: Record<string, string>
+  title: React.ReactNode
+  onChange: (value: string) => void
+  value: string
+  className?: string
+}) => {
+  return (
+    <div className={clsx(styles.tabs, props.className)}>
+      <Typography variant="body3" className={styles.tabsTitle}>
+        {props.title}
+      </Typography>
+      <div className={styles.tabsPaper}>
+        {Object.values(props.children).map((value) => (
+          <ButtonBase
+            key={value}
+            onClick={() => props.onChange(value)}
+            className={clsx(styles.tabsItem, {
+              [styles.tabsItemActive]: props.value === value,
+            })}
+          >
+            {value}
+          </ButtonBase>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export const StakingApyDialog: React.FC<StakingApyDialogProps> = (props) => {
-  const staked = bignumberUtils.eq(props.staked, 0) ? 1000 : props.staked
+  const [amount, setAmount] = useState('1000')
+  const [stakedFor, setStakedFor] = useState(StakedFor['1D'])
+  const [compoundingEvery, setCompoundingEvery] = useState(
+    CompoundingEvery.AUTO
+  )
 
-  const apr = Object.entries(props.apr).map(([title, apy]) => {
-    const apyMul = bignumberUtils.mul(apy, 100)
+  // const staked = bignumberUtils.eq(props.staked, 0) ? amount : props.staked
 
-    return {
-      title,
-      apy: apyMul,
-      perStaked: bignumberUtils.mul(apy, staked),
-    }
-  })
+  // const apr = Object.entries(props.apr).map(([title, apy]) => {
+  //   const apyMul = bignumberUtils.mul(apy, 100)
+
+  //   return {
+  //     title,
+  //     apy: apyMul,
+  //     perStaked: bignumberUtils.mul(apy, staked),
+  //   }
+  // })
+
+  const CompundingEveryComponents = {
+    [CompoundingEvery.AUTO]: (
+      <>
+        <Link
+          href="https://defihelper.medium.com/auto-staking-explained-da5fbab082e0"
+          target="_blank"
+          color="blue"
+          className={styles.howItWorks}
+        >
+          How auto-staking works?
+        </Link>
+        <Button color="green" onClick={props.onConfirm}>
+          Auto-stake
+        </Button>
+      </>
+    ),
+    [CompoundingEvery['1D']]: (
+      <>
+        <Button color="green" onClick={props.onConfirm}>
+          Auto-stake
+        </Button>
+      </>
+    ),
+    [CompoundingEvery['7D']]: (
+      <>
+        <Button color="green" onClick={props.onConfirm}>
+          Auto-stake
+        </Button>
+      </>
+    ),
+    [CompoundingEvery['14D']]: (
+      <>
+        <Button color="green" onClick={props.onConfirm}>
+          Auto-stake
+        </Button>
+      </>
+    ),
+  }
 
   return (
     <Dialog className={styles.root}>
-      <div className={styles.row}>
-        <Typography variant="body2">Timeframe</Typography>
-        <Typography variant="body2" align="right">
-          Apy
-        </Typography>
-        <Typography variant="body2" align="right">
-          Per ${bignumberUtils.format(staked)}
-        </Typography>
-      </div>
-      {apr.map((aprItem) => (
-        <div className={styles.row} key={aprItem.title}>
-          <Typography variant="body2">{aprItem.title}</Typography>
-          <Typography variant="body2" family="mono" align="right">
-            {bignumberUtils.formatMax(aprItem.apy, 10000)}%
-          </Typography>
-          <Typography variant="body2" family="mono" align="right">
-            ${bignumberUtils.format(aprItem.perStaked)}
-          </Typography>
-        </div>
-      ))}
+      <Typography
+        variant="body3"
+        family="mono"
+        transform="uppercase"
+        className={styles.title}
+      >
+        Roi calculator
+      </Typography>
+      <NumericalInput
+        value={amount}
+        onChange={(event) => setAmount(event.target.value)}
+        rightSide="$"
+        className={styles.mb16}
+        label="Amount"
+      />
+      <Tabs
+        title="Staked for"
+        onChange={setStakedFor}
+        value={stakedFor}
+        className={styles.mb16}
+      >
+        {StakedFor}
+      </Tabs>
+      <Tabs
+        title={
+          <>
+            Compounding every{' '}
+            <Icon icon="checkboxChecked" width="1em" height="1em" />
+          </>
+        }
+        onChange={setCompoundingEvery}
+        value={compoundingEvery}
+        className={styles.mb16}
+      >
+        {CompoundingEvery}
+      </Tabs>
+      {CompundingEveryComponents[compoundingEvery]}
     </Dialog>
   )
 }
